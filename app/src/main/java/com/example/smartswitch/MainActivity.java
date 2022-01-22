fe92ff889bf2d690d5a58dd80031da364c25c8b3
Richet-Barton <options3224@gmail.com>
2022-01-22 20:07:21 +0900

    버튼으로 HTTP 접속하여 LED 켜기/끄기 제어

--- app/src/main/java/com/example/smartswitch/MainActivity.java
+++ app/src/main/java/com/example/smartswitch/MainActivity.java
@@ -1,21 +1,23 @@
 package com.example.smartswitch;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.Switch;
-import android.widget.Toast;
+import android.webkit.WebView;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
     static int portNumber = 80;
+    private static long back_pressed;
 
     Switch alarmSwitch;
     LinearLayout alarmLayout;
     EditText etPortNumber;
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         loadView();
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    // 뒤로가기 버튼을 눌렀을 때
+    @Override
+    public void onBackPressed() {
+        if (back_pressed + 2000 > System.currentTimeMillis()){
+            super.onBackPressed();
+            finish();
+        }
+        else {
+            Toast.makeText(getBaseContext(), "한번 더 뒤로가기를 누르면 종료됩니다.", Toast.LENGTH_SHORT).show();
+            back_pressed = System.currentTimeMillis();
+        }
     }
 
     // 알람 스위치를 눌렀을 때
@@ -36,11 +58,13 @@ public class MainActivity extends AppCompatActivity {
     // ON 버튼을 눌렀을 때
     public void onClickButtonOn(View view) {
         Toast.makeText(getApplicationContext(), "ButtonOn is clicked.", Toast.LENGTH_SHORT).show();
+        webView.loadUrl("192.168.0.23/ledOn");
     }
 
     // OFF 버튼을 눌렀을 때
     public void onClickButtonOff(View view) {
         Toast.makeText(getApplicationContext(), "ButtonOff is clicked.", Toast.LENGTH_SHORT).show();
+        webView.loadUrl("192.168.0.23/ledOff");
     }
 
     // View 불러오기
@@ -48,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
         alarmSwitch = findViewById(R.id.alarmSwitch);
         alarmLayout = findViewById(R.id.alarmLayout);
         etPortNumber = findViewById(R.id.etPortNumber);
+        webView = findViewById(R.id.webView);
     }
 
     // 테스트
@@ -59,4 +84,23 @@ public class MainActivity extends AppCompatActivity {
             portNumber = 80;
         startActivity(intent);
     }
+
+    // 다이얼로그 출력
+    public void showErrorDialog(String message)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Error");
+        builder.setCancelable(false);
+        builder.setMessage(message);
+        builder.setPositiveButton("OK",  new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.create().show();
+    }
+
 }
+
+
